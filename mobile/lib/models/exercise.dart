6ab0694496e971a4495401e067736ba160d24f6b
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2023-01-16 15:42:04 -0500

    add exercise model

--- mobile/lib/models/exercise.dart
+++ mobile/lib/models/exercise.dart
@@ -2,7 +2,26 @@ class Exercise {
   String id;
   String name;
   int sets;
+  double weight;
   int reps;
-  String? mediaId;
-  Exercise(this.id, this.name, this.sets, this.reps, this.mediaId);
+  double time;
+  String? mediaLink;
+  String? notes;
+  Exercise(this.id, this.name, this.weight, this.sets, this.reps, this.time,
+      this.mediaLink, this.notes);
+}
+
+class ExerciseInProgress extends Exercise {
+  bool isDone;
+  ExerciseInProgress(
+      {required String id,
+      required String name,
+      required int sets,
+      double weight = 0,
+      int reps = 0,
+      double time = 0,
+      String mediaLink = "",
+      String notes = "",
+      this.isDone = false})
+      : super(id, name, weight, sets, reps, time, mediaLink, notes);
 }
