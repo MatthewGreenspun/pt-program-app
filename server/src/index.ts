e34afc8b6be0fc7e4faee82eb3e7f3968dcc023e
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2023-01-16 15:40:03 -0500

    setup express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,12 @@
+import express from "express";
+import usersRouter from "./routes/users.router";
+require("dotenv").config();
+const app = express();
 
-export function sayHelloWorld(world: string) {
-  return `Hello ${world}`;
-}
+app.use("/users", usersRouter);
+
+app.get("/", (req, res) => {
+  res.json({ ip: req.ip, headers: req.headers });
+});
+
+app.listen(process.env.PORT || 8080, () => console.log("server listening"));
