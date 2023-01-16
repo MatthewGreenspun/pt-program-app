bdbf29f981a1a9f9206da90193e50ac7218db487
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2023-01-16 15:39:21 -0500

    add password verification functions

---
+++ server/src/utils/crypto.ts
@@ -0,0 +1,21 @@
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 10;
+
+export async function genHash(password: string) {
+  const salt = await bcrypt.genSalt(SALT_ROUNDS);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+}
+
+export async function verifyPassword(password: string, hash: string) {
+  const isMatch = await bcrypt.compare(password, hash);
+  return isMatch;
+}
+
+export async function test() {
+  const hash = await genHash("test123");
+  console.log("hash: ", hash, hash.length);
+  const isMatch = await verifyPassword("mxsupersecurepassword123", hash);
+  console.log(isMatch);
+}
