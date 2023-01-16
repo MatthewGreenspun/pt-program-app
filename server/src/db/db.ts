64b55668f171bf87d5cd0b816a6f865794a404d4
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2023-01-16 15:38:35 -0500

    database connection setup

---
+++ server/src/db/db.ts
@@ -0,0 +1,13 @@
+import { Pool, QueryResultRow } from "pg";
+require("dotenv").config();
+
+const pool = new Pool({
+  connectionString: process.env.DB_URI,
+});
+
+export function query<T extends QueryResultRow>(
+  text: string,
+  params: Array<any>
+) {
+  return pool.query<T, any[]>(text, params);
+}
