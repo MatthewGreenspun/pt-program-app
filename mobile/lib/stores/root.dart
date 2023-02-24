ef368f844081ce378d0ae5d8ced8a312e3002084
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2023-02-24 09:20:11 -0500

    move add patients button into separate file

--- mobile/lib/stores/root.dart
+++ mobile/lib/stores/root.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:mobx/mobx.dart';
 import '../screens/exercises/exercises.dart';
+import '../screens/patients/add_patients_button.dart';
 import '../screens/patients/patients.dart';
 import '../screens/programs/add_programs_button.dart';
 import '../screens/programs/programs.dart';
